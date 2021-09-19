6ea52263deae32c384efbcd7fe8322dd37030a93
ViniciusNino <viniciussanvas@gmail.com>
2021-09-19 17:26:18 -0300

    refect autenticação usuário

--- MarketList_API/Controllers/UsuarioController.cs
+++ MarketList_API/Controllers/UsuarioController.cs
@@ -3,13 +3,14 @@ using System.Net;
 using System.Threading.Tasks;
 using MarketList_Business;
 using MarketList_Model;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MarketList_API.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
-
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioBusiness _usuarioBusiness;
@@ -20,24 +21,23 @@ namespace MarketList_API.Controllers
         }
 
         [HttpGet]
-        public int GetTeste()
-        {
-            return 1;
-        }
-
-        [HttpPost]
         [Route("Autenticar")]
-        public async Task<IActionResult> AutenticarUsuario(InformacaoAutenticacaoUsuarioDTO informacaoAutenticacaoUsuario)
+        public async Task<IActionResult> AutenticarUsuario(string usuario, string senha)
         {
-            // try
-            // {
+            var informacaoAutenticacaoUsuario = new InformacaoAutenticacaoUsuarioDTO
+            {
+                Senha = senha, 
+                Usuario = usuario
+            };
+            try
+            {
                 var usuarioAutenticado = await _usuarioBusiness.AutenticarUsuario(informacaoAutenticacaoUsuario);
                 return Ok(usuarioAutenticado);
-            // }
-            // catch (Exception ex)
-            // {
-            //     return BadRequest(ex);
-            // }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
